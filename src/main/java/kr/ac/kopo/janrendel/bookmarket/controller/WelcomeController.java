c819f0fd46adfc605237f84ab5988832adf2c043
janrendel <janrendel11@gmail.com>
2026-03-24 11:48:43 +0900

    4장 WelcomeController 추가 중

---
+++ src/main/java/kr/ac/kopo/janrendel/bookmarket/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.janrendel.bookmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/home")
+    public String WelcomeMethod() {
+        return "welcome";
+    }
+}
